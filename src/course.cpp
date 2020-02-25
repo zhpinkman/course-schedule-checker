c52030c001404fc059ab15ae5417ef653ec58598
AmirHossein Habibvand <ahhabibvand@gmail.com>
2020-02-25 13:43:19 +0330

    Add step 2 and 3 functionalities

--- src/course.cpp
+++ src/course.cpp
@@ -2,11 +2,16 @@
 #include <map>
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <algorithm>
 
 const int NOT_FOUND = -1;
+const float GROWTH_RATE = 1.05;
+const std::string FILE_PREFIX = "semester";
+const std::string FILE_SUFFIX = ".sch";
 
-WeekDay getWeekDayFromString(std::string weekDay)
+WeekDay
+getWeekDayFromString(std::string weekDay)
 {
     return std::map<std::string, WeekDay>{
         {"Sat", Sat},
@@ -17,6 +22,11 @@ WeekDay getWeekDayFromString(std::string weekDay)
         {"Thu", Thu},
         {"Fri", Fri}
     }.at(weekDay);
+}
+
+int maxAllowedUnits(Grade gpa)
+{
+    return gpa >= 17 ? 24 : 20;
 }
 
 Courses parseCourses(const CSVData &data)
@@ -142,44 +152,55 @@ CourseGrade parseCourseGrade(const CSVRow &row)
     return courseGrade;
 }
 
-void print(Time time)
+void print(const Time& time, std::ostream &stream = std::cout)
 {
-    std::cout << (time.hour < 10 ? "0" : "") << time.hour << ':' << (time.minute < 10 ? "0" : "") << time.minute;
+    stream << (time.hour < 10 ? "0" : "") << time.hour << ':' << (time.minute < 10 ? "0" : "") << time.minute;
 }
 
-void print(Session session)
+void print(const Session& session, std::ostream &stream = std::cout)
 {
-    std::cout << session.weekDay << ' ';
+    stream << session.weekDay << ' ';
     print(session.start);
-    std::cout << ' ';
+    stream << ' ';
     print(session.end);
 }
 
-void print(Course course, bool withDetails = false)
+void print(const Course& course, std::ostream &stream = std::cout, bool withDetails = false)
 {
-    std::cout << course.id;
+    stream << course.id;
     if (withDetails)
     {
-        std::cout << ' ' << course.units << ' ';
+        stream << ' ' << course.units << ' ';
         for (auto &&prerequisite : course.prerequisites)
         {
-            std::cout << prerequisite << ' ';
+            stream << prerequisite << ' ';
         }
-        std::cout << '\t' << course.name << "\t\t";
+        stream << '\t' << course.name << "\t\t";
         for (auto &&session : course.sessions)
         {
             print(session);
-            std::cout << ' ';
+            stream << ' ';
         }
     }
-    std::cout << std::endl;
+    stream << std::endl;
 }
 
-void print(Courses courses)
+void print(const Courses& courses, std::ostream &stream = std::cout, bool withDetails = false)
 {
     for (auto &&course : courses)
     {
-        print(course);
+        print(course, stream, withDetails);
+    }
+}
+
+void printToFile(std::string fileName, const Courses& courses)
+{
+    std::ofstream fout(fileName);
+    std::cout << fileName << ' ' << fout.is_open() << std::endl;
+    if (fout.is_open())
+    {
+        print(courses, fout);
+        fout.close();
     }
 }
 
@@ -252,24 +273,82 @@ bool compareCourseByUnitsAndName(const Course &lhs, const Course &rhs)
     return (lhs.units != rhs.units && lhs.units < rhs.units) || compareCourseByName(lhs, rhs);
 }
 
+int calculateNumberOfUnits(const Courses &courses)
+{
+    int result = 0;
+    for (auto &&course : courses)
+    {
+        result += course.units;
+    }
+    return result;
+}
+
+bool canTakeCourse(const Courses &nextTermCourses, Grade currentGPA, Course course)
+{
+    int numberOfUnits = calculateNumberOfUnits(nextTermCourses) + course.units;
+    return numberOfUnits <= maxAllowedUnits(currentGPA);
+}
 Courses findNextTermCourses(const Courses &availableCourses, Grade currentGPA)
 {
     Courses nextTermCourses;
-    std::cout << currentGPA << std::endl;
+    Courses::const_iterator courseIter = availableCourses.begin();
+    while(courseIter != availableCourses.end())
+    {
+        Course course = *courseIter;
+        if (canTakeCourse(nextTermCourses, currentGPA, course))
+        {
+            nextTermCourses.push_back(course);
+        }
+        courseIter++;
+    }
     return nextTermCourses;
+}
+
+bool hasPassedAllCourses(const Courses &courses, Student student)
+{
+    for (auto &&course : courses)
+    {
+        if (!hasPassed(student, course))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void passCourses(Student &student, const Courses &courses, Grade grade)
+{
+    for (auto &&course : courses)
+    {
+        student.courseGrades[course.id] = grade;
+    }
 }
 
 void runBoostan(const Courses &courses, Student student, int step)
 {
-    Courses availableCourses = findAvailableCourses(courses, student);
     if (step == 1)
     {
+        Courses availableCourses = findAvailableCourses(courses, student);
         std::sort(availableCourses.begin(), availableCourses.end(), compareCourseByName);
         print(availableCourses);
     }
     else if (step == 2)
     {
+        Courses availableCourses = findAvailableCourses(courses, student);
         std::sort(availableCourses.begin(), availableCourses.end(), compareCourseByUnitsAndName);
         print(findNextTermCourses(availableCourses, calculateGPA(courses, student)));
+    }
+    else if(step == 3)
+    {
+        int iteration = 1;
+        while (!hasPassedAllCourses(courses, student))
+        {
+            Courses availableCourses = findAvailableCourses(courses, student);
+            std::sort(availableCourses.begin(), availableCourses.end(), compareCourseByUnitsAndName);
+            Grade currentGPA = calculateGPA(courses, student);
+            Courses nextTermCourses = findNextTermCourses(availableCourses, currentGPA);
+            passCourses(student, nextTermCourses, currentGPA * GROWTH_RATE);
+            printToFile(FILE_PREFIX + std::to_string(iteration++) + FILE_SUFFIX, nextTermCourses);
+        }
     }
 }
